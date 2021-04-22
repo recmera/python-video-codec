7388230d6dee3393c109605e1f68f7edddd5d2cf
Eduardo Hopperdietzel <eduardo.hopperdietzel@alumnos.uach.cl>
2021-04-22 13:49:50 -0400

    Pruebas filtro FIR, FFT y IIR para eliminar sinusoide

--- mycodec.py
+++ mycodec.py
@@ -1,16 +1,18 @@
 import numpy as np
 import cv2 as cv
 
+# Nuestras librerías
+from FiltroSinusoide import filtradoIIR,filtradoFIR,filtradoFFT
+
 def denoise(frame):
-    #
-    # Implementa la función que elimina el ruido de la imagen
-    #
-    return frame
+    #return filtradoFIR(frame)
+    #return filtradoFFT(frame)
+    return filtradoIIR(frame)
 
 def code(frame):
     #
     # Implementa en esta función el bloque transmisor: Transformación + Cuantización + Codificación de fuente
-    #    
+    #
     return frame
 
 
@@ -18,8 +20,8 @@ def decode(message):
     #
     # Reemplaza la linea 24...
     #
-    frame = np.frombuffer(bytes(memoryview(message)), dtype='uint8').reshape(480, 848)
+    frame = np.frombuffer(bytes(memoryview(message)), dtype='uint8').reshape(480, 848, order='F')
     #
     # ...con tu implementación del bloque receptor: decodificador + transformación inversa
-    #    
+    #
     return frame
--- transmitter.py
+++ transmitter.py
@@ -12,10 +12,10 @@ vid_height, vid_width = cam.get_resolution()
 print("Resolución del video: {0}x{1}p".format(vid_width, vid_height))
 print("Cuadros por segundo: {0}".format(cam.get_fps()))
 
+
 for frame in cam:
-    frame_denoise = denoise(frame)  
+    frame_denoise = denoise(frame)
     message = code(frame_denoise)
     socket.send(message)
     status = socket.recv()
-    # print(status)
-    
+    #print(status)
